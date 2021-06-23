e8028f771d83b40a2b802f61958075b0a7760202
Osanda Deshan <osanda.deshan@gmail.com>
2021-06-23 12:11:48 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -19,28 +19,36 @@ app.get('/', function (req, res) {
         var nameArray = name.split(" ");
 
         var content = dom.window.document.documentElement.outerHTML;
-        var doc = new xmldom().parseFromString(content)
-        var nodes = xpath.select("//h2[normalize-space()='Experience']/following::h3[1]", doc)
+        var doc = new xmldom().parseFromString(content);
+        var interests = dom.window.document.querySelectorAll('pv-entity__summary-title-text');
+        var inArr = [];
+        dom.window.document.querySelectorAll('pv-entity__summary-title-text').forEach(element => {
+            inArr.push(element.textContent);
+        });
+
+        console.log(inArr.toString());
 
         res.status(200).json({
-            pageTitle: dom.window.document.querySelector('title').textContent,
             firstName: nameArray[0],
             lastName: nameArray[nameArray.length - 1],
-            jobTitle: dom.window.document.querySelector('div.text-body-medium').textContent.trim(),
-            company: "",
-            description: "",
+            headline: dom.window.document.querySelector('.text-body-medium.break-words').textContent.trim(),
+            jobTitle: xpath.select("string(//h2[normalize-space()='Experience']/following::h3[1])", doc).trim(), //dom.window.document.querySelector('div.text-body-medium').textContent.trim()
+            company: xpath.select("string(//h2[normalize-space()='Experience']/following::p[2])", doc).trim(),
+            about: xpath.select("string(//h2[normalize-space()='About']/following::div[1])", doc).trim(),
             contactInformation: {
-                email: "",
-                twitter: ""
+                linkedinProfileUrl: xpath.select("string(//h3[normalize-space()='Your Profile']/following::a[1])", doc),
+                website: xpath.select("string(//h3[normalize-space()='Website']/following::a[1])", doc),
+                phone: xpath.select("string(//h3[normalize-space()='Phone']/following::span[1])", doc),
+                email: xpath.select("string(//h3[normalize-space()='Email']/following::a[1])", doc)
             },
-            birthday: "",
-            workedCompanies: {
+            birthday: xpath.select("string(//h3[normalize-space()='Birthday']/following::span[1])", doc),
+            workedCompanies: [{
                 companyName: "",
                 started: "",
                 end: "",
                 description: "",
-                jobTitle: nodes[0].firstChild.data
-            },
+                jobTitle: ""
+            }],
             education: [],
             certifications: [],
             references: [{
@@ -52,7 +60,7 @@ app.get('/', function (req, res) {
                 description: "",
                 date: ""
             }],
-            interests: [],
+            interests: inArr.toString(),
             mutualConnections: []
         })
 
